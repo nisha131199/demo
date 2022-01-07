8fd70c5c1fa1a28e90df0f90f8c5567382e04f94
Nisha <nisha130599@gmail.com>
2022-01-07 11:53:10 +0530

    database changed

--- src/main/kotlin/com/example/demo/user/User.kt
+++ src/main/kotlin/com/example/demo/user/User.kt
@@ -1,26 +1,24 @@
 package com.example.demo.user
 
-import com.example.demo.user.database.MySqlDb
+import com.example.demo.user.database.MySQLite
 import java.sql.ResultSet
 import java.sql.Statement
-import java.text.SimpleDateFormat
-import java.util.Date
 import kotlin.collections.HashMap
 
 class User {
     private val res = HashMap<String, Any>()
-    private val db = MySqlDb()
+    private val db = MySQLite()
+    private val connection = db.connection()
+    private val statement = connection.createStatement() as Statement
 
     private lateinit var query: String
 
     private fun isReadyToRegister(data: DataModel): String {
         var msg = "new user"
 
-        val connection = db.getConnection()
-        val statement = connection.createStatement() as Statement
         var result: ResultSet
 
-        if (data.password.isNotBlank() && data.confirmPassword.isNotBlank() && data.password != data.confirmPassword)
+        if (data.password.isNotBlank() && data.confirm_password.isNotBlank() && data.password != data.confirm_password)
             msg = "Those passwords didn't matched"
 
         if (data.email.isNotBlank()) {
@@ -30,8 +28,8 @@ class User {
                 msg = "This email is already registered here"
         }
 
-        if (data.userName.isNotBlank()) {
-            query = "select user_name from user where user_name='${data.userName}'"
+        if (data.user_name.isNotBlank()) {
+            query = "select user_name from user where user_name='${data.user_name}'"
             result = statement.executeQuery(query)
             if (result.next())
                 msg = "username already taken"
@@ -40,36 +38,28 @@ class User {
     }
 
     fun register(data: DataModel): HashMap<String, Any> {
-        val connection = db.getConnection()
-        val statement = connection.createStatement() as Statement
-
         try {
             val msg = isReadyToRegister(data)
 
             if (msg.equals("new user")) {
-                val firstName = data.firstName?.trim()
-                val middleName = data.middleName?.trim()
-                val lastName = data.lastName?.trim()
+                val firstName = data.first_name?.trim()
+                val middleName = data.middle_name?.trim()
+                val lastName = data.last_name?.trim()
                 val email = data.email.trim()
-                val phoneNumber = data.phoneNumber?.trim()
-                val userName = data.userName.trim()
+                val phoneNumber = data.phone_number?.trim()
+                val userName = data.user_name.trim()
                 val password = data.password.trim()
-                query = "insert into user (first_name,middle_name,last_name,email,phone_number,user_name,password) " +
-                        "value('$firstName','$middleName','$lastName','$email','$phoneNumber','$userName','$password')"
+                query = "insert into user (first_name, middle_name, last_name, email, phone_number, user_name, password) " +
+                        "values('$firstName','$middleName','$lastName'," +
+                        "'$email','$phoneNumber','$userName','$password')"
 
-                if (statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS) > 0) {
-                    val d = HashMap<String, Any>()
-                    val key=statement.generatedKeys
-                    if (key.next())
-                        d["id"] = key.getInt(1)
-
+                if (statement.execute(query)) {
                     res["status"] = true
                     res["message"] = "user have been added successfully."
-                    res["data"] = d
+                }else{
+                    res["status"] = true
+                    res["message"] = "user not added."
                 }
-
-                res["status"] = true
-                res["message"] = "user have been added successfully."
 
             } else {
                 res["status"] = false
@@ -83,14 +73,11 @@ class User {
     }
 
     fun update(data: DataModel): HashMap<String, Any> {
-        val connection = db.getConnection()
-        val statement = connection.createStatement() as Statement
-
         try {
-            val timeStamp= SimpleDateFormat("YYYY-MM-DD hh:mm:ss").format(Date())
-            query = "update user set first_name='${data.firstName}',middle_name='${data.middleName}'," +
-                    "last_name='${data.lastName}',email='${data.email}',phone_number='${data.phoneNumber}'," +
-                    "user_name='${data.userName}',password='${data.password}',updated_at='$timeStamp' " +
+            //val timeStamp= SimpleDateFormat("YYYY-MM-DD hh:mm:ss").format(Date())
+            query = "update user set first_name='${data.first_name}',middle_name='${data.middle_name}'," +
+                    "last_name='${data.last_name}',email='${data.email}',phone_number='${data.phone_number}'," +
+                    "user_name='${data.user_name}',password='${data.password}'"+
                     "where id=${data.id}"
 
             if (statement.executeUpdate(query) > 0) {
@@ -105,9 +92,6 @@ class User {
     }
 
     fun delete(uid:Int): HashMap<String, Any> {
-        val connection = db.getConnection()
-        val statement = connection.createStatement() as Statement
-
         try {
             query = "delete from user where id=${uid}"
             if (statement.executeUpdate(query) > 0) {
@@ -122,32 +106,29 @@ class User {
     }
 
     fun login(login:LoginModel): HashMap<String, Any> {
-        val connection = db.getConnection()
-        val statement = connection.createStatement() as Statement
-
         try {
             query = "select * from user where user_name='${login.userName}' and password='${login.password}'"
             val result=statement.executeQuery(query)
             if (result.next()) {
                 val data=HashMap<String,Any>()
                 data["id"]=result.getString(1)
-                data["firstName"]=result.getString(2)
+                /*data["firstName"]=result.getString(2)
                 data["middleName"]=result.getString(3)
-                data["lastName"]=result.getString(4)
+                data["lastName"]=result.getString(4)*/
                 val firstName=result.getString(2)
                 val middleName=result.getString(3)
                 val lastName=result.getString(4)
-                data["fullName"]="$firstName $middleName $lastName"
+                data["Name"]="$firstName $middleName $lastName"
                 data["email"]=result.getString(5)
-                data["phoneNumber"]=result.getString(6)
-                data["userName"]=result.getString(7)
+                data["phone_number"]=result.getString(6)
+                data["user_name"]=result.getString(7)
                 data["password"]=result.getString(8)
                 res["status"] = true
                 res["message"] = "Successfully SignIn"
                 res["data"]=data
             }else{
                 res["status"] = false
-                res["message"] = "Invalid Username and Password"
+                res["message"] = "Invalid Username or Password"
             }
         } catch (e: Exception) {
             res["status"] = false
@@ -161,11 +142,11 @@ class LoginModel(var userName:String, var password:String)
 
 data class DataModel(
         var id: Int?,
-        var firstName: String?,
-        var middleName: String?,
-        var lastName: String?,
+        var first_name: String?,
+        var middle_name: String?,
+        var last_name: String?,
         var email: String,
-        var phoneNumber: String?,
-        var userName: String,
+        var phone_number: String?,
+        var user_name: String,
         var password: String,
-        var confirmPassword: String)
+        var confirm_password: String)
---
+++ src/main/kotlin/com/example/demo/user/database/MySQLite.kt
@@ -0,0 +1,14 @@
+package com.example.demo.user.database
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class MySQLite {
+    private lateinit var conn: Connection
+
+    fun connection(): Connection{
+        Class.forName("org.sqlite.JDBC")
+        conn = DriverManager.getConnection("jdbc:sqlite:F:/Database/dbms_testing.db")
+        return conn
+    }
+}
