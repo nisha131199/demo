ab6653ac7d9f94c0afef3fec404da0d76a3247e5
Nisha <nisha130599@gmail.com>
2022-01-12 16:34:08 +0530

    updated data

---
+++ src/main/kotlin/com/example/demo/user/database/MySQLClass.kt
@@ -0,0 +1,12 @@
+package com.example.demo.user.database
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class MySQLClass {
+    //database>data source>mysql>advance>enableTLSProtocol TLSv1,TLSv1.1,TLSv1.2,TLSv1.3
+    fun getSQLConnection(): Connection {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
+        return DriverManager.getConnection("jdbc:mysql://mysql-database-1.cf7tlvfbvdxr.us-east-2.rds.amazonaws.com:3306/user_data", "admin123", "admin123")
+    }
+}
